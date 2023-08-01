22212aa4aaaa2fc71b552b05d85e84d38e6a317e
KAKAN <22027824+theKAKAN@users.noreply.github.com>
2023-08-01 09:48:17 +0530

    Reverse the order

--- 23-08-01_LAB2/q2.c
+++ 23-08-01_LAB2/q2.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[]) {
 
     unsigned int num = atoi(argv[1]);
 
-    unsigned int firstByte = num & 0xFF,
-                secondByte = (num>>8) & 0xFF,
-                thirdByte = (num>>16) & 0xFF,
-                fourthByte = (num>>24) & 0xFF;
+    unsigned int fourthByte = num & 0xFF,
+                thirdByte = (num>>8) & 0xFF,
+                secondByte = (num>>16) & 0xFF,
+                firstByte = (num>>24) & 0xFF;
 
     printf("Original number: %u\n", num);
     printf("The bytes are: %u %u %u %u", firstByte, secondByte, thirdByte, fourthByte);
